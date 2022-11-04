532a1f7e0f4bf13aacc669cc6fade5d87b6c46ba
TEISSIER Nathan <luigivampa12@protonmail.ch>
2022-11-04 15:12:09 +0100

    ntn

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { AfterContentChecked, Component, OnInit } from '@angular/core';
-import SwiperCore, { SwiperOptions, Autoplay } from 'swiper';
-SwiperCore.use([Autoplay]);
+import SwiperCore, { SwiperOptions, Autoplay, Pagination, EffectCoverflow } from 'swiper';
+SwiperCore.use([Autoplay, Pagination, EffectCoverflow]);
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,12 @@ export class HomePage implements OnInit, AfterContentChecked {
   loc = 'Locating..';
   banners = [];
   bannerConfig: SwiperOptions;
+  categoryConfig: SwiperOptions;
+  restaurantConfig: SwiperOptions;
+  categories = [];
+  favorites = [];
+  offers = [];
+  nearby = [];
   constructor() { }
 
   ngOnInit() {
@@ -20,18 +27,167 @@ export class HomePage implements OnInit, AfterContentChecked {
       {banner: 'assets/dishes/1.jpg'},
       {banner: 'assets/dishes/cab.jpg'},
     ];
+    this.categories = [
+      { id: 1, name: 'North Indian', image: 'assets/dishes/nan.jpg' },
+      { id: 2, name: 'Italian', image: 'assets/dishes/pasta.jpg' },
+      { id: 3, name: 'Chowmein', image: 'assets/dishes/chowmein.jpg' },
+      { id: 4, name: 'South Indian', image: 'assets/dishes/dosa.jpg' },
+      { id: 5, name: 'Mexican', image: 'assets/dishes/dol.jpg' },
+    ];
+    this.favorites = [
+      {
+        id: '1',
+        cover: 'assets/dishes/restaurant.jpg',
+        name: 'Stayfit',
+        cuisines: [
+          'Indian',
+          'Italian',
+          'Mexican'
+        ],
+        rating: 5,
+        delivery_time: 25,
+        distance: 2.5,
+        price: 100,
+        latitude: 0,
+        longitude: 0
+      },
+      {
+        id: '2',
+        cover: 'assets/dishes/2.jpg',
+        name: 'Stayfit1',
+        cuisines: [
+          'Italian',
+          'Mexican',
+          'Chinese'
+        ],
+        rating: 5,
+        delivery_time: 25,
+        distance: 2.5,
+        price: 100
+      },
+      {
+        id: '3',
+        cover: 'assets/dishes/3.jpg',
+        name: 'Kolkata Roll',
+        cuisines: [
+          'Italian',
+          'Mexican'
+        ],
+        rating: 5,
+        delivery_time: 25,
+        distance: 2.5,
+        price: 100
+      },
+    ];
+    this.offers = [
+      {
+        id: '1',
+        cover: 'assets/dishes/3.jpg',
+        name: 'Kolkata Roll',
+        cuisines: [
+          'Italian',
+          'Mexican'
+        ],
+        rating: 5,
+        delivery_time: 25,
+        distance: 2.5,
+        price: 100
+      },
+      {
+        id: '2',
+        cover: 'assets/dishes/2.jpg',
+        name: 'Stayfit1',
+        cuisines: [
+          'Italian',
+          'Mexican',
+          'Chinese'
+        ],
+        rating: 5,
+        delivery_time: 25,
+        distance: 2.5,
+        price: 100
+      },
+      {
+        id: '3',
+        cover: 'assets/dishes/restaurant.jpg',
+        name: 'Stayfit',
+        cuisines: [
+          'Indian',
+          'Italian',
+          'Mexican'
+        ],
+        rating: 5,
+        delivery_time: 25,
+        distance: 2.5,
+        price: 100,
+        latitude: 0,
+        longitude: 0
+      },
+    ];
+    this.nearby = [
+      {
+        id: '1',
+        cover: 'assets/dishes/restaurant.jpg',
+        name: 'Stayfit',
+        cuisines: [
+          'Indian',
+          'Italian',
+          'Mexican'
+        ],
+        rating: 5,
+        delivery_time: 25,
+        distance: 2.5,
+        price: 100,
+        latitude: 0,
+        longitude: 0
+      },
+      {
+        id: '2',
+        cover: 'assets/dishes/2.jpg',
+        name: 'Stayfit1',
+        cuisines: [
+          'Italian',
+          'Mexican',
+          'Chinese'
+        ],
+        rating: 5,
+        delivery_time: 25,
+        distance: 2.5,
+        price: 100
+      },
+      {
+        id: '3',
+        cover: 'assets/dishes/3.jpg',
+        name: 'Kolkata Roll',
+        cuisines: [
+          'Italian',
+          'Mexican'
+        ],
+        rating: 5,
+        delivery_time: 25,
+        distance: 2.5,
+        price: 100
+      },
+    ];
   }
 
   ngAfterContentChecked(): void {
     this.bannerConfig ={
       slidesPerView:1.2,
-      spaceBetween: 50,
+      spaceBetween: 10,
       centeredSlides: true,
-      initialSlide: 1,
+      initialSlide: this.banners?.length > 1 ? 1 : 0,
       autoplay: {
         delay: 3000
-      }
+      },
+      pagination:{ clickable: true},
+      effect: 'coverflow'
+    };
+    this.categoryConfig = {
+      slidesPerView: 3.5
+    };
+    this.restaurantConfig = {
+      slidesPerView: 1.1
     };
   }
-
 }
