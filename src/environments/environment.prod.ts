4d9006762bbc5590f9a96cc4c14ac908dea38c0a
TEISSIER Nathan <luigivampa12@protonmail.ch>
2022-11-04 09:46:46 +0100

    ntn

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD1_8ReU7QQcPbIlqgdCLz7TWb_XZNti74',
+    authDomain: 'luigi-deliveries.firebaseapp.com',
+    projectId: 'luigi-deliveries',
+    storageBucket: 'luigi-deliveries.appspot.com',
+    messagingSenderId: '918201136724',
+    appId: '1:918201136724:web:581dc4c086ddb9b46f9fc1',
+    measurementId: 'G-SFRSZLZ0HT'
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD1_8ReU7QQcPbIlqgdCLz7TWb_XZNti74',
+    authDomain: 'luigi-deliveries.firebaseapp.com',
+    projectId: 'luigi-deliveries',
+    storageBucket: 'luigi-deliveries.appspot.com',
+    messagingSenderId: '918201136724',
+    appId: '1:918201136724:web:581dc4c086ddb9b46f9fc1',
+    measurementId: 'G-SFRSZLZ0HT'
+  },
+
 };
 
 /*
